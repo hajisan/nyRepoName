3f867fa0ad0e64bdfcc92387f01db0cf9384e9a8
Nima <nimaginobilisalami@gmail.com>
2024-11-29 14:38:58 +0100

    Tilføjet String og rettet småfejl

--- ProjectName/src/Main.java
+++ ProjectName/src/Main.java
@@ -1,5 +1,10 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo World");
+        System.out.println("Hello World");
+
+        String nima = "Nima";
+        String andreas = "Andreas";
+        String joakim = "Joakim";
+        String sofie = "sofie";
     }
 }
